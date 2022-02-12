0ea2b281ca870fae786c353535857e241c9d584a
DESKTOP-G1CVF9J\Hasan TOMA <hasabantv1999@gmail.om>
2022-02-12 23:54:14 +0300

    mahmut

--- lib/product/components/list_tile/switch_list_tile.dart
+++ lib/product/components/list_tile/switch_list_tile.dart
@@ -1,29 +1,36 @@
+// ignore_for_file: no_logic_in_create_state
+
 import 'package:flutter/material.dart';
 import 'package:hamster/core/extension/context_extension.dart';
 import 'package:hamster/core/init/theme/color_palette.dart';
 
 class CustomSwitchListTile extends StatefulWidget {
-  const CustomSwitchListTile({Key? key, this.title,required this.boolValue}) : super(key: key);
+  const CustomSwitchListTile({Key? key, this.title, required this.boolValue})
+      : super(key: key);
   final String? title;
   final bool boolValue;
 
   @override
-  _CustomSwitchListTileState createState() => _CustomSwitchListTileState(boolValue);
+  _CustomSwitchListTileState createState() =>
+      _CustomSwitchListTileState(boolValue);
 }
 
 class _CustomSwitchListTileState extends State<CustomSwitchListTile> {
   bool visibility = false;
-  _CustomSwitchListTileState(bool value){
+  _CustomSwitchListTileState(bool value) {
     visibility = value;
   }
-
 
   @override
   Widget build(BuildContext context) {
     return SwitchListTile(
-      title: Text(widget.title ?? "",style: context.textTheme.titleMedium?.copyWith(color: Colors.black,fontWeight: FontWeight.w600),),
+      title: Text(
+        widget.title ?? "",
+        style: context.textTheme.titleMedium
+            ?.copyWith(color: Colors.black, fontWeight: FontWeight.w600),
+      ),
       value: visibility,
-      onChanged: (bool newValue){
+      onChanged: (bool newValue) {
         visibility = !visibility;
         setState(() {});
       },
